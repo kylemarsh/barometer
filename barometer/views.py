135af3d76d286d2f5ba336e2b86bcef69e0dabd5
Kyle Marsh <kyle.marsh@dreamhost.com>
2012-12-31 13:13:25 -0800

    Added .swp to the .gitignore Added some error pages

--- barometer/views.py
+++ barometer/views.py
@@ -1,17 +1,26 @@
-from flask import abort, render_template
+from flask import render_template
 
 from barometer import app
 from barometer.models import Amount, Bottle, Category, Subcategory
 
 
 @app.route("/")
+@app.route("/<category>")
 def index(category=None):
     if category:
         results = Bottle.query.filter_by(category=category).all()
         if not results:
-            results = Bottle.query.filter_by(category=category).all()
-        if not results:
-            abort(404)
+            results = Bottle.query.filter_by(subcategory=category).all()
     else:
         results = Bottle.query.all()
-    return render_template('index.html', bottles=results)
+    return render_template('index.html', bottles=results, category=category)
+
+
+@app.route("/add")
+def add():
+    return render_template('unimplemented.html', page='Add')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
