ee970caf347cddcace1fe3f78c99c48dac17b147
Kyle Marsh <kyle.marsh@dreamhost.com>
2012-12-31 17:07:04 -0800

    implemented new category/subcategory functionality
    Adding bottles lets you add new categories and subcategories now

--- barometer/views.py
+++ barometer/views.py
@@ -20,15 +20,27 @@ def index(category=None):
 def add():
     if request.method == 'POST':
         form = request.form
-        NewBottle = Bottle(
-            form['description'],
-            form['category'].lower(),
-            form['subcategory'].lower(),
-            form['size'],
-            form['amount'].lower())
-        db.session.add(NewBottle)
-        db.session.commit()
-        flash('%s was added to your inventory' % NewBottle)
+        category = form['category']
+        subcategory = form['subcategory']
+        try:
+            if form['category'] == 'new':
+                category = add_category(form['new_category'])
+            if form['subcategory'] == 'new':
+                subcategory = add_subcategory(
+                    category,
+                    form['new_subcategory'])
+            NewBottle = Bottle(
+                form['description'],
+                category,
+                subcategory,
+                form['size'],
+                form['amount'].lower())
+            db.session.add(NewBottle)
+            db.session.commit()
+            flash('%s was added to your inventory' % NewBottle)
+        except CategoryError:
+            flash('%s is not a valid category' % category)
+
     lists = dict(
         categories=Category.query.all(),
         subcategories=Subcategory.query.all(),
@@ -44,3 +56,36 @@ def delete():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+
+def add_category(new_category):
+    new_category = new_category.strip().lower()
+    if Category.query.filter_by(category=new_category).first():
+        app.logger.debug('category %s already exists' % new_category)
+        return new_category
+    db.session.add(Category(category=new_category))
+    db.session.commit()
+    return new_category
+
+
+def add_subcategory(category, new_subcategory):
+    new_subcategory = new_subcategory.strip().lower()
+    if not Category.query.filter_by(category=category).first():
+        # we don't have the category...abort
+        app.logger.error('category %s does not exist; ' % category +
+            'cannot create subcategory')
+        raise CategoryError
+    if Subcategory.query.filter_by(subcategory=new_subcategory).first():
+        app.logger.debug('subcategory %s.%s already exists' %
+        (category, new_subcategory))
+        return new_subcategory
+    db.session.add(Subcategory(
+        category=category,
+        subcategory=new_subcategory))
+    db.session.commit()
+    return new_subcategory
+
+
+class CategoryError(Exception):
+    """Raised when something tries to use a nonexistant category
+    """
